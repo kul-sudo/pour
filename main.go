2c822e20f2f13012afde2fc9a969d35b78edbcb6
kul-sudo <ulyanov.kirill@protonmail.com>
2025-07-12 16:05:17 +0300

    tim I think it's close to working

--- main.go
+++ main.go
@@ -1,21 +1,69 @@
 package main
 
 import (
-	"pour/bootstrap"
-	"pour/node"
-	"pour/seeder"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	config, err := bootstrap.ConfigGen()
-	if err != nil {
-		return
+	// config, err := bootstrap.ConfigGen()
+	// if err != nil {
+	// 	return
+	// }
+
+	// ffmpeg -i 19.mp4 -f segment -segment_time 10 -reset_timestamps 1 -c copy -segment_format_options movflags=frag_keyframe+empty_moov pour/chunks/%d.mp4
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			files, _ := ioutil.ReadDir("chunks/")
+
+			maxNum := -1
+			maxFile := ""
+			for _, f := range files {
+				filename := strings.TrimSuffix(f.Name(), ".mp4")
+				if num, err := strconv.Atoi(filename); err == nil {
+					if num > maxNum {
+						maxNum = num
+						maxFile = f.Name()
+					}
+				}
+			}
+
+			fmt.Println(maxFile)
+			maxFile = "2.mp4"
+
+			http.ServeFile(w, r, filepath.Join("chunks", maxFile))
+		})
+		http.ListenAndServe("localhost:8080", nil)
+	}()
+
+	go func() {
+		http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {
+			t, err := template.ParseFiles("html/node.html")
+			if err != nil {
+				fmt.Printf("failed to parse node dashboard html")
+				return
+			}
+			t.Execute(w, nil)
+		})
+		// fs := http.FileServer(http.Dir("/home/user/pour/chunks"))
+		// http.Handle("/chunks/", http.StripPrefix("/chunks", fs))
+		http.ListenAndServe("localhost:8082", nil)
+
+	}()
+
+	for {
+
 	}
 
-	switch config.Mode {
-	case "seeder":
-		seeder.Setup(&config)
-	case "node":
-		node.Setup(&config)
-	}
+	// switch config.Mode {
+	// case "seeder":
+	// 	seeder.Setup(&config)
+	// case "node":
+	// 	node.Setup(&config)
+	// }
 }
